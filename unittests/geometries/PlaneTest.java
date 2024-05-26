10336e1b02a3350189c8f309062c390aceabe6d7
moshegoodman <moshegoodman7@gmail.com>
2024-05-26 18:25:05 +0300

    the tests are nearly complete cylinder should correct the points and the rest you might want to get rid of some of the tests

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -9,8 +9,18 @@ class PlaneTest {
     Point p1         = new Point(1, 2, 3);
     Point  p2         = new Point(2, 4, 6);
     Point  p3         = new Point(2, 4, 5);
+    Point p1PlusP2 = new Point(3,6,9);
+     void testConstructor() {
 
+        //validates that when two points are the same it will throw an exception
+         assertThrows(IllegalArgumentException.class, ()->new Plane(p1,p1,p2),
+                 "does not throw an exception when two points are the same");
 
+         //validated that it throws an exception when all three points are on a single line
+         assertThrows(IllegalArgumentException.class, ()->new Plane(p1,p2,p1PlusP2),
+                 "does not throw an exception when all three points are on a single line");
+
+    }
 
     @Test
     void testGetNormal() {
@@ -21,8 +31,10 @@ class PlaneTest {
         assertEquals(1,pl1.getNormal().length(),"Normal is not a unit vector");
 
         //  validates that the normal is orthogonal to the vectors contained on the plane
-        assertEquals(0,(p1.subtract(p2)).dotProduct(pl1.getNormal()),"Normal is not orthogonal to plane");
-        assertEquals(0,(p1.subtract(p3)).dotProduct(pl1.getNormal()),"Normal is not orthogonal to plane");
+        assertEquals(0,(p1.subtract(p2)).dotProduct(pl1.getNormal()),
+                "Normal is not orthogonal to plane");
+        assertEquals(0,(p1.subtract(p3)).dotProduct(pl1.getNormal()),
+                "Normal is not orthogonal to plane");
 
 
         //validates that when point is off the plane program will throw an exception
--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SphereTest {
     primitives.Point p1 = new primitives.Point(3,0,0);
+    primitives.Point p2 = new primitives.Point(2,0,0);
+    Sphere s1 = new Sphere(2.0,new primitives.Point(0,0,0));
 
 
     /**
@@ -13,8 +15,21 @@ class SphereTest {
      */
     @Test
     void testGetNormal() {
-        Sphere s1 = new Sphere(2.0,new primitives.Point(0,0,0));
+
+        //validates that when point is off the plane program will throw an exception
         assertThrows(IllegalArgumentException.class,()->s1.getNormal(p1),
                 "does not throw exception when point is not on the sphere ");
+
+
+        //validates that the normal function returns a unit vector
+        assertEquals(1,s1.getNormal(p2).length(),0.0001,
+                "the normal of a sphere does not return a unit vector");
+
+
+
+
     }
+
+
+
 }
