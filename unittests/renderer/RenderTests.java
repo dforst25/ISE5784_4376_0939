5f3c6c32a1d0281260ed5c1988bb163014913553
דוד פורסט <dforst@g.jct.ac.il>
2024-07-05 12:49:42 +0300

    first part of level 6

--- unittests/renderer/RenderTests.java
+++ unittests/renderer/RenderTests.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.Test;
 import geometries.*;
 import lighting.AmbientLight;
 import primitives.*;
-import renderer.*;
 import scene.Scene;
 
-/** Test rendering a basic image
- * @author Dan */
+/**
+ * Test rendering a basic image
+ * @author Dan
+ */
 public class RenderTests {
     /** Scene of the tests */
     private final Scene          scene  = new Scene("Test scene");
@@ -22,24 +23,52 @@ public class RenderTests {
             .setVpDistance(100)
             .setVpSize(500, 500);
 
-    /** Produce a scene with basic 3D model and render it into a png image with a
-     * grid */
+    /**
+     * Produce a scene with basic 3D model and render it into a png image with a
+     * grid
+     */
     @Test
     public void renderTwoColorTest() {
-        scene.geometries.add(new Sphere(50d, new Point(0, 0, -100) ),
-                new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100),
-                        new Point(-100, 100, -100)).setEmission(new Color(GREEN)), // up
+        scene.geometries.add(new Sphere(50d, new Point(0, 0, -100)),
+                new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
                 // left
                 new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100),
-                        new Point(-100, -100, -100)).setEmission(new Color(RED)), // down
+                        new Point(-100, -100, -100)), // down
                 // left
-                new Triangle(new Point(100, 0, -100), new Point(0, -100, -100),
-                        new Point(100, -100, -100)).setEmission(new Color(BLUE))); // down
-        scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.2)));
+                new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // down
+        scene.setAmbientLight(new AmbientLight(new Color(255, 191, 191), Double3.ONE))
+                .setBackground(new Color(75, 127, 90));
 
         // right
         Camera builtCamera = camera.setImageWriter(
                 new ImageWriter("base render test", 1000, 1000)).build();
+        builtCamera.renderImage();
+        builtCamera.printGrid(100, new Color(YELLOW));
+        builtCamera.writeToImage();
+    }
+
+    // For stage 6 - please disregard in stage 5
+    /**
+     * Produce a scene with basic 3D model - including individual lights of the
+     * bodies and render it into a png image with a grid
+     */
+    @Test
+    public void renderMultiColorTest() {
+        scene.geometries.add( // center
+                new Sphere(50, new Point(0, 0, -100)),
+                // up left
+                new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100))
+                        .setEmission(new Color(GREEN)),
+                // down left
+                new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100))
+                        .setEmission(new Color(RED)),
+                // down right
+                new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))
+                        .setEmission(new Color(BLUE)));
+        scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.2, 0.2, 0.2))); //
+
+        Camera builtCamera = camera.setImageWriter(
+                new ImageWriter("color render test", 1000, 1000)).build();
         builtCamera.renderImage();
         builtCamera.printGrid(100, new Color(WHITE));
         builtCamera.writeToImage();
@@ -50,7 +79,7 @@ public class RenderTests {
     public void basicRenderXml() {
         // enter XML file name and parse from XML file into scene object
         // using the code you added in appropriate packages
-        // .....
+        // ...
         // NB: unit tests is not the correct place to put XML parsing code
 
         Camera builtCamera = camera.setImageWriter(
@@ -59,4 +88,5 @@ public class RenderTests {
         builtCamera.printGrid(100, new Color(YELLOW));
         builtCamera.writeToImage();
     }
+
 }
